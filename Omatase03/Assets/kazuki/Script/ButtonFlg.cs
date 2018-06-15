0603cc0d72e297813a94157efa39fc9e327fc65a
kazuki1505 <kazuki-o-17e@stu.kbc.ac.jp>
2018-06-15 12:00:06 +0900

    Helpシーンを移しました。

---
+++ Omatase03/Assets/kazuki/Script/ButtonFlg.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonFlg : MonoBehaviour {
+
+    GameObject LeftButtonFlg;
+    GameObject RightButtonFlg;
+    public void RCloseButton()
+    {
+        RightButtonFlg.SetActive(false);
+        LeftButtonFlg.SetActive(true);
+    }
+    public void LCloseButton()
+    {
+        LeftButtonFlg.SetActive(false);
+        RightButtonFlg.SetActive(true);
+    }
+
+    // Use this for initialization
+    void Start ()
+    {
+        LeftButtonFlg = GameObject.Find("LeftButton").gameObject;
+        RightButtonFlg = GameObject.Find("RightButton").gameObject;
+        LeftButtonFlg.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+		
+	}
+}
